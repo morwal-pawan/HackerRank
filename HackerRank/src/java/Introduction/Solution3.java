162143104217853661aed774985f35a3560a312c
morwal <morwal@192.168.0.109>
2018-02-25 16:21:10 -0800

    Java If-Else

---
+++ HackerRank/src/java/Introduction/Solution3.java
@@ -0,0 +1,27 @@
+package java.Introduction;
+
+import java.util.Scanner;
+
+public class Solution3 {
+
+    public static void main(String[] args) {
+
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        String ans="";
+        if(n%2==1){
+          ans = "Weird";
+        }
+        else{
+              if(n >=2 && n <5)
+              ans = "Not Weird";
+
+              else if(n >=6 && n <=20)
+                      ans = "Weird";
+              else if(n >20)
+                ans = "Not Weird";
+        }
+        System.out.println(ans);
+
+    }
+}
