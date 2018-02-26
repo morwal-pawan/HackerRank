9ca12078c91dbfa3f31ac8e696e0ffd2f3488798
morwal <morwal@192.168.0.109>
2018-02-25 16:35:34 -0800

    Java End-of-file

---
+++ HackerRank/src/java/Introduction/Solution9.java
@@ -0,0 +1,19 @@
+package java.Introduction;
+
+import java.util.Scanner;
+
+public class Solution9 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int i=1;
+        while(sc.hasNext())
+        {
+            System.out.println(i +" "+ sc.nextLine());
+           i++;
+       }  
+    }
+}
+
+
+
