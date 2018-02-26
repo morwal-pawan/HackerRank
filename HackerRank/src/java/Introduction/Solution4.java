6cc0fb73351fa5c7127a5fcbf1d940a48a4f2bf1
morwal <morwal@192.168.0.109>
2018-02-25 16:22:51 -0800

    Java Stdin and Stdout II by Shafaet

---
+++ HackerRank/src/java/Introduction/Solution4.java
@@ -0,0 +1,17 @@
+package java.Introduction;
+
+import java.util.Scanner;
+
+public class Solution4 {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int i = Integer.parseInt(scan.nextLine());
+        double d = Double.parseDouble(scan.nextLine());
+        String s = scan.nextLine();
+        System.out.println("String: " + s);
+        System.out.println("Double: " + d);
+        System.out.println("Int: " + i);
+    }
+}
+
