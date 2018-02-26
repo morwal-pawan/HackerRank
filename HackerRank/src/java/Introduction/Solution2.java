1be50cf7607d455970cb0a79329cede41c744776
morwal <morwal@192.168.0.109>
2018-02-25 16:18:49 -0800

    Java Stdin and Stdout I

---
+++ HackerRank/src/java/Introduction/Solution2.java
@@ -0,0 +1,17 @@
+package java.Introduction;
+
+import java.util.Scanner;
+
+public class Solution2 {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        for(int i=0 ; i<3 ;i++)
+            {
+             int a = scan.nextInt();
+             System.out.println(a);
+        }
+       
+         
+    }
+}
