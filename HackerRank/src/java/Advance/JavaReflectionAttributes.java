c487ff50008831ad90b45e514479b03ff49aab28
morwal <morwal@192.168.0.106>
2018-02-26 22:47:37 -0800

    Java Reflection - Attributes

---
+++ HackerRank/src/java/Advance/JavaReflectionAttributes.java
@@ -0,0 +1,22 @@
+package java.Advance;
+
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Collections;
+
+    public class JavaReflectionAttributes {
+    public static void main(String[] args) throws ClassNotFoundException{
+        Class student =Class.forName("Student");
+        Method[] methods = student.getDeclaredMethods();
+       
+
+        ArrayList<String> methodList = new ArrayList<>();
+        for(Method method: methods ){
+            methodList.add(method.getName());
+        }
+        Collections.sort(methodList);
+        for(String name: methodList){
+            System.out.println(name);
+        }
+    }
+  }
