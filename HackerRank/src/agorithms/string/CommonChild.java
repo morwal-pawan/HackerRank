279a0a4fca9e209efa5d054f7a3ddd7fbc526e07
morwal <morwal@192.168.0.102>
2018-03-25 02:10:46 -0700

    Sherlock and the Valid String

--- HackerRank/src/agorithms/string/CommonChild.java
+++ HackerRank/src/agorithms/string/CommonChild.java
@@ -1,32 +1,46 @@
 package agorithms.string;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class CommonChild {
 
-    private static int lcs[][];
-    public static int commonChilds(String str1,int length1,String str2,int length2)
+   public static String isValid(String str)
 	{
-		if(length1 == 0 || length2 ==0) return 0;
-		if(lcs[length1][length2] !=0) return lcs[length1][length2];
-		if(str1.charAt(length1-1) == str2.charAt(length2-1))	
-			return lcs[length1][length2] = commonChilds(str1, length1-1, str2, length2-1) + 1;
-		else  return lcs[length1][length2] = Math.max(commonChilds(str1, length1-1, str2, length2), commonChilds(str1, length1, str2, length2-1));	
-	}
-	
-	public static int commonChild(String str1,String str2) {
 		
-		int length1 = str1.length();
-		int length2 = str2.length();
-		lcs = new int[length1+1][length2+1];
-		return commonChilds(str1, length1, str2, length2);
+		int length = str.length();
+		int count=0;
+		int min=0,min_count=0;
+		int[] countChars = new int[26];
+		for (int str_index = 0; str_index < length; str_index++) {
+			countChars[str.charAt(str_index)-'a']++;
+		}
+       
+        Arrays.sort(countChars);
+       int i;
+	     for ( i = 0; i < countChars.length; i++) {
+	    	 if(countChars[i]!=0)
+	    	 {
+	    		 min = countChars[i];
+	    		 break;
+	    	 }
+		}
+		
+		for (int index = i; index < countChars.length; index++) {
+				int temp =Math.abs(countChars[index] - min);
+				if(temp >=1) count+=temp;
+                if(min ==countChars[index])min_count++;
+			
+		}
+		if(count==1 || count==0 ||min==1&&min_count==1) return  "YES";
+		return "NO";
+		
 	}
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        String s1 = in.next();
-        String s2 = in.next();
-        int result = commonChild(s1, s2);
+        String s = in.next();
+        String result = isValid(s);
         System.out.println(result);
     }
 }
