83ce5239dcab35ecfcd1b5b13709af05758cd635
morwal <morwal@192.168.0.102>
2018-03-25 02:11:58 -0700

    String Construction

--- HackerRank/src/agorithms/string/CommonChild.java
+++ HackerRank/src/agorithms/string/CommonChild.java
@@ -1,46 +1,31 @@
 package agorithms.string;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class CommonChild {
 
-   public static String isValid(String str)
+   public static int stringConstruction(String str)
 	{
-		
-		int length = str.length();
-		int count=0;
-		int min=0,min_count=0;
-		int[] countChars = new int[26];
-		for (int str_index = 0; str_index < length; str_index++) {
-			countChars[str.charAt(str_index)-'a']++;
-		}
-       
-        Arrays.sort(countChars);
-       int i;
-	     for ( i = 0; i < countChars.length; i++) {
-	    	 if(countChars[i]!=0)
-	    	 {
-	    		 min = countChars[i];
-	    		 break;
-	    	 }
-		}
-		
-		for (int index = i; index < countChars.length; index++) {
-				int temp =Math.abs(countChars[index] - min);
-				if(temp >=1) count+=temp;
-                if(min ==countChars[index])min_count++;
+		int count[] = new int[256];
+		int count1=0;
+		for (int index = 0; index < str.length(); index++) 
+			count[str.charAt(index)]++;
 			
+		for (int i = 0; i < count.length; i++) {
+			if(count[i]!=0) 
+				count1++;
 		}
-		if(count==1 || count==0 ||min==1&&min_count==1) return  "YES";
-		return "NO";
-		
+		return count1;
 	}
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        String s = in.next();
-        String result = isValid(s);
-        System.out.println(result);
+        int q = in.nextInt();
+        for(int a0 = 0; a0 < q; a0++){
+            String s = in.next();
+            int result = stringConstruction(s);
+            System.out.println(result);
+        }
+        in.close();
     }
 }
